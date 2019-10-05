a192ac2558191051d37fac3f2cd10797ea06166f
kuviman <kuviman@gmail.com>
2019-10-05 14:23:25 +0400

    Wall text

--- src/map.rs
+++ src/map.rs
@@ -10,6 +10,12 @@ impl Map {
         vec2(16, 16)
     }
     pub fn text_at(&self, pos: Vec2<f32>) -> Option<String> {
+        fn close(pos: f32, size: usize) -> bool {
+            pos.abs() < 0.5 || (pos - size as f32).abs() < 0.5
+        }
+        if close(pos.x, self.size().x) || close(pos.y, self.size().y) {
+            return Some("Wall".to_owned());
+        }
         None
     }
 }
