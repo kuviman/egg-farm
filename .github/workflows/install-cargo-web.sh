b69e83f88b472561b323c067a29e07d18ca4bfbf
kuviman <kuviman@gmail.com>
2020-04-05 13:16:45 +0400

    Fix cargo-web installation

--- .github/workflows/install-cargo-web.sh
+++ .github/workflows/install-cargo-web.sh
@@ -1,5 +1,3 @@
-set -e
-
 CARGO_WEB_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/koute/cargo-web/releases/latest)
 CARGO_WEB_VERSION=$(echo $CARGO_WEB_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 if [ "$(uname -s)" == "Darwin" ]; then
@@ -11,8 +9,8 @@ CARGO_WEB_URL="https://github.com/koute/cargo-web/releases/download/$CARGO_WEB_V
 
 
 echo "Downloading cargo-web from: $CARGO_WEB_URL"
-curl -LsS $CARGO_WEB_URL | gzip -d > cargo-web
+curl -L $CARGO_WEB_URL | gzip -d > cargo-web
 chmod +x cargo-web
 
-mkdir -p /usr/local/cargo/bin
-mv cargo-web /usr/local/cargo/bin/
+mkdir -p ~/.cargo/bin
+mv cargo-web ~/.cargo/bin
